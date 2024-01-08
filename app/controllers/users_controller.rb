3740ff5d4fac690198a447d6d3f2817fa91b48e0
Tim <todongoonditi@gmail.com>
2024-01-09 01:26:05 +0300

    user CRUD edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: uder
+        else
+            render json: {error: "user not found"}, status: :not_found
+        end
+    end
+
+    def create
+        user = User.create(username: params[:username], email: params[:email], profile_image: params[:profile_image], phone_number: params[:phone_number], password: params[:password])
+        if user.valid?
+            render json: {success: "user created successfully"}
+        else
+            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+        end               
+    end
+
+    def update
+        user = user.find_by(id: params[:id])
+
+        if user
+            user.update(username: params[:username], email: params[:email], profile_image: params[:profile_image], phone_number: params[:phone_number], password: params[:password])
+            render json: {success: "user updated successful"}, status: :created
+        else
+            render json: {error: "user not found"}, status: :not_found
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        if user
+            user.destroy
+            render json: {success: "user deleted successful"}, status: :created
+        else
+            render json: {error: "user you are trying to delete does not exist"}, status: :not_found
+        end        
+    end
+    end
 end
--- db/migrate/20240108220348_create_users.rb
+++ db/migrate/20240108220348_create_users.rb
@@ -5,7 +5,8 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :email
       t.string :profile_image
       t.integer :phone_number
-      t.boolean :is_admin
+      t.string :password_digest
+      t.boolean :is_admin, default: false
 
       t.timestamps
     end
