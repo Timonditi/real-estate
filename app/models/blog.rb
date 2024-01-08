d2f122acdcf7c3bde697994a91ca175f14ea54b6
Tim <todongoonditi@gmail.com>
2024-01-09 01:29:26 +0300

    relationship between user and blog created

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,8 @@
 class Blog < ApplicationRecord
+    belongs_to :user
+
+    validates :title, presence: true, uniqueness: true
+    validates :image, presence: true
+    validates :content, presence: true
+    validates :user_id, presence: true
 end
