b0c22d9327a1969b0cd0dcf3c194ea243003f905
Tim <todongoonditi@gmail.com>
2024-01-10 01:47:36 +0300

    relationship between user and properties created

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,10 @@
 class Property < ApplicationRecord
+    belongs_to :user
+
+    validates :title, presence: true, uniqueness: true
+    validates :image, presence: true
+    validates :description, presence: true
+    validates :amount, presence: true
+    validates :bedrooms, presence: true
+    validates :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :blogs
+    has_many :properties
 
     has_secure_password
 
